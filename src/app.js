e957b6eeaf476df2b6347b98fcb0ba4ea9a985cb
JaZahn <jcleveng@fas.harvard.edu>
2015-09-29 13:26:41 -0400

    first

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('shuffling', []);
+
+app.controller('FormController', [function(){
+
+
+}]);
+
+app.controller('TabController', ['guests', '$scope', function(guests, $scope){
+
+}]);
