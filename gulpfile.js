e17dd40ee9addae99664cfc3fcf9e861efcc781a
Scott Pullen <s.pullen05@gmail.com>
2015-10-11 20:11:37 -0400

    Adding tasks to gulp, creating tests directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
 var gulp = require('gulp');
+
+var Server = require('karma').Server;
+
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var connect = require('gulp-connect');
+var del = require('del');
+
+
